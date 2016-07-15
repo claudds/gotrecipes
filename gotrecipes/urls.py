b33c4826b86a78afd97e3ebd61cdfb97bc4e0b79
Claudia Della Serra <claudiadellaserra@gmail.com>
2016-07-14 21:49:54 -0400

    Added django form to add a new recipe

--- gotrecipes/urls.py
+++ gotrecipes/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
 	url(r'^$', views.index, name='index'),
+	url(r'^new/$', views.new_recipe, name='new_recipe'),
+#	url(r'^login/$', 'django.contrib.auth.views.login'),
+#	url(r'^logout/$, 'django.contrib.auth.views.logout'),
 ]
--- gotrecipes/views.py
+++ gotrecipes/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+from .forms import RecipeForm
 
 # Create your views here.
 def index(request):
 	return render(request, 'gotrecipes/index.html', {})
+
+def new_recipe(request):
+	if request.method == "POST":
+		form = RecipeForm(request.POST)
+		if form.is_valid():
+			recipe = form.save(commit=False)
+			recipe.save()
+	else:
+		form = RecipeForm()
+	return render(request, 'gotrecipes/recipe_edit.html', {'form': form})
