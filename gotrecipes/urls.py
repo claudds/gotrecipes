2cb4d371b68b60c6f7873127971f6f360ec337ea
Claudia Della Serra <claudiadellaserra@gmail.com>
2016-09-13 22:43:38 -0400

    Added recipe-search function

--- gotrecipes/urls.py
+++ gotrecipes/urls.py
@@ -4,6 +4,8 @@ from . import views
 urlpatterns = [
 	url(r'^$', views.index, name='index'),
 	url(r'^new/$', views.new_recipe, name='new_recipe'),
+	url(r'^search/$', views.search_recipe, name='search_recipe'),
+	url(r'^search_result/(?P<st>\w+)/$', views.search_result, name='search_result'),
 	url(r'^recipe/(?P<pk>\d+)/$', views.recipe_detail, name='recipe_detail'),
 	url(r'^recipe/(?P<pk>\d+)/edit/$', views.recipe_edit, name='recipe_edit'),
 	url(r'^recipe/(?P<pk>\d+)/remove/$', views.recipe_delete, name='recipe_delete'),
--- gotrecipes/views.py
+++ gotrecipes/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from .forms import RecipeForm
 from .models import Recipe
 from django.contrib.auth.decorators import login_required
-
+from django.db.models import Q
 
 # Create your views here.
 def index(request):
@@ -23,6 +23,16 @@ def new_recipe(request):
 	else:
 		form = RecipeForm()
 	return render(request, 'gotrecipes/recipe_edit.html', {'form': form})
+
+def search_recipe(request):
+	if request.method == "POST":
+		return redirect('search_result', st=request.POST.get('search-term', ""))
+	return render(request, 'gotrecipes/search_recipe.html')
+
+def search_result(request, st):
+	userRecipes = Recipe.objects.order_by('title')
+	results = Recipe.objects.filter(ingredients__icontains=st).filter(title__icontains=st)
+	return render(request, 'gotrecipes/search_result.html', {'recipes': results})
 
 @login_required
 def recipe_edit(request, pk):
